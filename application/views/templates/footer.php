f0e89abeb0717a5c40a2f1005db725034968b327
anggakp007 <putraangga2810@gmail.com>
2023-11-18 21:57:46 +0700

    Mengganti bagian footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-   <strong>Copyright &copy; 2023 <a href="<?= base_url('dashboard') ?>">DiamondStore</a>.</strong>
+   <strong>Copyright &copy; 2023 <a href="<?= base_url('dashboard') ?>">SPRS</a>.</strong>
    All rights reserved.
    <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 1.0.0
