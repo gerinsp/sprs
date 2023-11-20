049d7db9486c6bcc379d804d38aac5875ed56c82
gerinsp <denapratama7@gmail.com>
2023-11-20 23:10:08 +0700

    hapus peminjaman ruangan dari dashboard pegawai

--- application/views/pages/pegawai/dashboard.php
+++ application/views/pages/pegawai/dashboard.php
@@ -46,35 +46,6 @@
                     </div>
                 </div>
 
-                <!-- Earnings (Monthly) Card Example -->
-                <div class="col-xl-6 col-md-6 mb-4">
-                    <div
-                        class="card border-left-success shadow h-100 py-2"
-                    >
-                        <div class="card-body">
-                            <div
-                                class="row no-gutters align-items-center"
-                            >
-                                <div class="col mr-2">
-                                    <div
-                                        class="text-xs font-weight-bold text-primary text-uppercase mb-1"
-                                    >
-                                        Peminjaman Ruangan
-                                    </div>
-                                    <div
-                                        class="h1 mb-0 font-weight-bold"
-                                    >
-                                        <?= $ruangan ?>
-                                    </div>
-                                </div>
-                                <div class="col-auto">
-                                    <i class="nav-icon fas fa-building fa-2x text-primary"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
             </div>
         </div>
     </div>
